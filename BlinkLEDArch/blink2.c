55873cea8685346a97ac5dcd2928da692d7a08cd
Robbie Leslie <leslier@lafayette.edu>
2024-10-08 22:27:11 -0400

    Got print working and added basicmath from MiBench

--- BlinkLEDArch/blink2.c
+++ BlinkLEDArch/blink2.c
@@ -7,16 +7,25 @@
 #include "pico/stdlib.h"
 #include "hardware/structs/pio.h"
 #include "hardware/structs/otp.h"
+#include <stdio.h>
 
 #define ARCHSEL_STATUS 0x15c
 
 
 int main() {
 
+    stdio_init_all();
+
     //Check the architecture of the RP2350
     //uint32_t archReg = *(volatile uint32_t *)(OTP_BASE + ARCHSEL_STATUS);
     uint32_t archReg = otp_hw->archsel_status;
     bool arch = (archReg & 0x1); //0 is arm and 1 is risc-v
+
+    if(arch){
+        printf("Hello World from RISC-V\n");
+    } else {
+        printf("Hello World from ARM\n");
+    }
 
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
