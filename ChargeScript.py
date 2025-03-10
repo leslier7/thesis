c162c216c828dfa9d98d6aa5424aebf4c28d5c73
Robbie Leslie <leslier@lafayette.edu>
2025-03-10 00:05:36 -0400

    Added script to read the charge from the PPK2 CSV files

---
+++ ChargeScript.py
@@ -0,0 +1,46 @@
+import csv
+import argparse
+import numpy as np
+
+def main(file_name):
+    with open(file_name, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        instances = []
+        current_instance = {'time': [], 'current': []}
+        for row in reader:
+            timestamp = float(row['Timestamp(ms)'])
+            current = float(row['Current(uA)'])
+            if row['D0'] == '1':
+                if not current_instance['time']:
+                    current_instance['start_time'] = timestamp
+                current_instance['time'].append(timestamp)
+                current_instance['current'].append(current)
+            else:
+                if current_instance['time']:
+                    instances.append(current_instance)
+                    current_instance = {'time': [], 'current': []}
+        if current_instance['time']:
+            instances.append(current_instance)
+
+        total_time = 0
+        total_current = 0
+        total_charge = 0
+        for index, instance in enumerate(instances, start=1):
+            time = instance['time']
+            current = instance['current']
+            time_diff = time[-1] - time[0]
+            total_time += time_diff
+            total_current += sum(current)
+            charge = np.trapezoid(current, time) / 1000
+            total_charge += charge
+            print(f'Instance {index} time: {time_diff} ms')
+            print(f'Instance current: {sum(current)} uA')
+            print(f'Instance charge: {charge} uC')
+            print('-------------------')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process a CSV file.')
+    parser.add_argument('file_name', type=str, help='The CSV file to process')
+    args = parser.parse_args()
+    main(args.file_name)
